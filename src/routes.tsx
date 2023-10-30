6c3e5e753759c5a43a5af31598579a86c2044296
austin-builds <austin.31@quarkonix.com>
2023-10-30 18:28:52 +0900

    fix: duplicated error

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,12 @@
 import { Navigate, createBrowserRouter } from 'react-router-dom';
 import Airdrop from './pages/airdrop';
-import { Faucet } from './pages/faucet';
+import Faucet from './pages/faucet';
 import Pool from './pages/pool';
 import PoolV2 from './pages/poolV2';
 import PoolV3 from './pages/poolV3';
 import Trade from './pages/trade';
 import TradeV2 from './pages/tradeV2';
 import TradeV3 from './pages/tradeV3';
-import Airdrop from './pages/airdrop';
 
 const router = createBrowserRouter([
   {
