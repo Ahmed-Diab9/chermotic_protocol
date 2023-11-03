dcb31e712fe36f35a3667825aed778cfc1b3b9ca
austin-builds <austin.31@quarkonix.com>
2023-11-03 19:50:14 +0900

    feat: check if user can use faucet by timestamps

--- src/hooks/useFaucet.ts
+++ src/hooks/useFaucet.ts
@@ -68,6 +68,60 @@ export const useFaucet = (props: UseFaucet) => {
     return faucetData;
   });
 
+  const checkTimeLeft = useCallback(async () => {
+    if (isNil(address) || isNil(faucetData)) {
+      return;
+    }
+    const timeLeftPromise = faucetData.map(async ({ token, amount, minInterval }) => {
+      const tokenContract = getContract({
+        abi: testSettlementTokenABI,
+        address: token.address,
+        publicClient: client.publicClient,
+        walletClient: client.walletClient,
+      });
+      const lastTimestamp = await tokenContract.read.lastFaucetTimestamp([address]);
+      const nextTimestamp = lastTimestamp + minInterval;
+      const isFaucetEnabled = lastTimestamp === 0n || Number(nextTimestamp) * 1000 < Date.now();
+      if (isFaucetEnabled) {
+        return {
+          isActive: true,
+          label: `Receive ${formatDecimals(amount, token.decimals, 0)}${token.name}`,
+          tokenName: token.name,
+        };
+      } else {
+        const timeLeft = Math.round(Number(nextTimestamp) * 1000 - Date.now());
+        const remainingDate = new Date(timeLeft);
+        const remainingHours = remainingDate.getUTCHours().toString().padStart(2, '0');
+        const remainingMinutes = remainingDate.getUTCMinutes().toString().padStart(2, '0');
+        return {
+          isActive: false,
+          label: `${remainingHours}h ${remainingMinutes}m Left`,
+          tokenName: token.name,
+        };
+      }
+    });
+    const timeLeft = await PromiseOnlySuccess(timeLeftPromise);
+    return timeLeft.reduce((record, timeItem) => {
+      record[timeItem.tokenName] = timeItem;
+      return record;
+    }, {} as Record<string, FaucetButtonState>);
+  }, [faucetData, address, client]);
+
+  useEffect(() => {
+    const timerId = setInterval(async () => {
+      const timeLeft = await checkTimeLeft();
+
+      setButtonStates(timeLeft);
+    }, 1000);
+
+    checkTimeLeft().then((nextStates) => {
+      setButtonStates(nextStates);
+    });
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [checkTimeLeft]);
 
   useError({ error });
 
