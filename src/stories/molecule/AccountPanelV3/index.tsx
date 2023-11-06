85972b6b758133dc99d9d093681167ad4af75aa7
dia.nn <dia.nn@quarkonix.com>
2023-11-06 11:34:27 +0900

    design: wording line break

--- src/stories/molecule/AccountPanelV3/index.tsx
+++ src/stories/molecule/AccountPanelV3/index.tsx
@@ -79,7 +79,8 @@ export const AccountPanelV3 = (props: AccountPanelV3Props) => {
           </article>
           <div className="my-7">
             <p className="text-primary-light">
-              This process may take approximately 10 seconds or so. Please wait a moment.
+              This process may take approximately 10 seconds or so. <br />
+              Please wait a moment.
             </p>
           </div>
           <div className="text-center">
