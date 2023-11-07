7990eac5849781c90c69c298c753ef1a03df213a
austin-builds <austin.31@quarkonix.com>
2023-11-07 23:44:04 +0900

    fix: show total margin from positions

--- src/hooks/useMargins.ts
+++ src/hooks/useMargins.ts
@@ -5,26 +5,29 @@ import { usePositions } from './usePositions';
 import { useSettlementToken } from './useSettlementToken';
 
 export function useMargins() {
-  const { currentPositions } = usePositions();
+  const { positions } = usePositions();
   const { balances } = useChromaticAccount();
   const { currentToken } = useSettlementToken();
 
   const [totalBalance, totalAsset] = useMemo(() => {
+    const filteredPositions = positions?.filter(
+      (position) => position.tokenAddress === currentToken?.address
+    );
     if (isNil(balances) || isNil(currentToken) || Object.keys(balances).length <= 0) {
       return [0n, 0n];
     }
     const balance = balances[currentToken.address];
-    if (isNil(currentPositions)) {
+    if (isNil(filteredPositions)) {
       return [balance, balance];
     }
-    const [totalCollateral, totalCollateralAdded] = currentPositions.reduce(
+    const [totalCollateral, totalCollateralAdded] = filteredPositions.reduce(
       (record, position) => {
         return [record[0] + position.collateral, record[1] + position.collateral + position.pnl];
       },
       [0n, 0n]
     );
     return [balance + totalCollateral, balance + totalCollateralAdded];
-  }, [balances, currentToken, currentPositions]);
+  }, [balances, currentToken, positions]);
 
   const totalMargin = useMemo(() => {
     if (isNil(balances) || isNil(currentToken)) {
