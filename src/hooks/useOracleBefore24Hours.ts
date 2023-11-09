09dcaed37b6dc2eb5186ddcfcd3ad1535794e3ab
austin-builds <austin.31@quarkonix.com>
2023-11-09 18:51:01 +0900

    fix: remove unuseful search

--- src/hooks/useOracleBefore24Hours.ts
+++ src/hooks/useOracleBefore24Hours.ts
@@ -1,6 +1,7 @@
 import { isNil } from 'ramda';
 import { useMemo } from 'react';
 import useSWR from 'swr';
+import { Address } from 'wagmi';
 import { ORACLE_PROVIDER_DECIMALS } from '~/configs/decimals';
 import { Market } from '~/typings/market';
 import { OracleVersion } from '~/typings/oracleVersion';
@@ -10,7 +11,9 @@ import { divPreserved } from '~/utils/number';
 import { useChromaticClient } from './useChromaticClient';
 import { useError } from './useError';
 
-type OracleWithMarket = OracleVersion & { marketAddress: string };
+const VERSION_INTERVAL = 700n;
+
+type OracleWithMarket = OracleVersion & { marketAddress: Address };
 
 export const useOracleBefore24Hours = ({ market }: { market?: Market }) => {
   const { isReady, client } = useChromaticClient();
@@ -23,54 +26,15 @@ export const useOracleBefore24Hours = ({ market }: { market?: Market }) => {
     error,
     mutate: _mutate,
   } = useSWR(isReady && checkAllProps(fetchKey) && fetchKey, async ({ market: trimmedMarket }) => {
-    let searchIndex = 0;
     const marketOracle = await client.market().getCurrentPrice(trimmedMarket.address);
-    const market = { ...trimmedMarket, oracleValue: marketOracle };
-    const currentVersion = market.oracleValue.version;
-    const oracleProvider = await client.market().contracts().oracleProvider(market.address);
-    if (currentVersion <= 0n) {
-      return;
-    }
+    console.log(marketOracle, 'curr');
+    const oracleProvider = await client.market().contracts().oracleProvider(trimmedMarket.address);
 
-    const [fromOracle, endOracle] = await Promise.allSettled([
-      oracleProvider.read.atVersion([0n]),
-      oracleProvider.read.atVersion([currentVersion]),
+    const endOracle = await oracleProvider.read.atVersion([
+      marketOracle.version - VERSION_INTERVAL,
     ]);
-    if (fromOracle.status === 'fulfilled' && endOracle.status === 'fulfilled') {
-      const { timestamp: fromTimestamp } = fromOracle.value;
-      const { timestamp: endTimestamp } = endOracle.value;
-      const fromDate = new Date(Number(fromTimestamp) * 1000);
-      const endDate = new Date(Number(endTimestamp) * 1000);
-      if (fromDate.setDate(fromDate.getDate() + 1) > endDate.getTime()) {
-        return;
-      }
-    }
 
-    const searchVersion = async (
-      minVersion: bigint,
-      maxVersion: bigint
-    ): Promise<OracleWithMarket | undefined> => {
-      if (searchIndex > 16) {
-        if (import.meta.env.DEV) {
-          throw new Error('Execution counts exceeded');
-        }
-        return;
-      }
-      searchIndex++;
-      const averageVersion = (minVersion + maxVersion) / 2n;
-      const averageOracle = await oracleProvider.read.atVersion([averageVersion]);
-      const oracleDiff = Number(market.oracleValue.timestamp - averageOracle.timestamp);
-      if (oracleDiff < 60 * 60 * 24) {
-        await new Promise((resolve) => setTimeout(resolve, 400));
-        return searchVersion(minVersion, averageVersion);
-      }
-      if (oracleDiff > 60 * 60 * 24 + 60 * 10) {
-        await new Promise((resolve) => setTimeout(resolve, 400));
-        return searchVersion(averageVersion, maxVersion);
-      }
-      return { ...averageOracle, marketAddress: market.address };
-    };
-    return searchVersion(0n, market.oracleValue.version);
+    return { ...endOracle, marketAddress: trimmedMarket.address };
   });
 
   const { isLoading, changeRate } = useMemo(() => {
