e389c4532769f3fba7faeeb95a3e69fac0f83ef0
austin-builds <austin.31@quarkonix.com>
2023-11-17 17:52:08 +0900

    fix: remove unused functions

--- src/utils/market.ts
+++ src/utils/market.ts
@@ -1,7 +1,5 @@
 import { isNil, isNotNil } from 'ramda';
-import { Address } from 'wagmi';
 import { Market, MarketLike } from '~/typings/market';
-import { OracleVersion } from '~/typings/oracleVersion';
 
 export const trimMarket = (market?: Market): MarketLike | undefined => {
   if (isNil(market)) {
@@ -12,30 +10,4 @@ export const trimMarket = (market?: Market): MarketLike | undefined => {
 };
 export const trimMarkets = (markets: Market[] = []): MarketLike[] => {
   return markets.map(trimMarket).filter((market): market is MarketLike => isNotNil(market));
-};
-
-export const compareMarkets = (previousMarkets: Market[], currentMarkets: Market[]) => {
-  const reducedPrevious = previousMarkets.reduce((reduced, market) => {
-    reduced[market.address] = market.oracleValue;
-    return reduced;
-  }, {} as Record<Address, OracleVersion>);
-  const reducedCurrent = currentMarkets.reduce((reduced, market) => {
-    reduced[market.address] = market.oracleValue;
-    return reduced;
-  }, {} as Record<Address, OracleVersion>);
-
-  for (let index = 0; index < currentMarkets.length; index++) {
-    const marketAddress = currentMarkets[index].address;
-    if (isNil(marketAddress)) {
-      continue;
-    }
-
-    const previousVersion = reducedPrevious[marketAddress];
-    const currentVersion = reducedCurrent[marketAddress];
-
-    if (previousVersion.version < currentVersion.version) {
-      return true;
-    }
-  }
-  return false;
 };
