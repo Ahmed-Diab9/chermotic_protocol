5f506db9bb73fe59dabc1debeed6ec9449354aa8
dia.nn <dia.nn@quarkonix.com>
2023-11-21 11:17:52 +0900

    fix: ChartLabel color

--- src/stories/atom/ChartLabel/index.tsx
+++ src/stories/atom/ChartLabel/index.tsx
@@ -4,7 +4,6 @@ import './style.css';
 
 interface ChartLabelProps {
   label?: string;
-  color?: 'primary' | string;
   translucent?: boolean;
   className?: string;
   thumbClass?: string;
@@ -12,16 +11,14 @@ interface ChartLabelProps {
 }
 
 export const ChartLabel = (props: ChartLabelProps) => {
-  const { label, color = 'primary', translucent, className, thumbClass, isLoading } = props;
+  const { label, translucent, className, thumbClass, isLoading } = props;
 
   return (
     <SkeletonElement isLoading={isNotNil(isLoading) && isLoading} width={80}>
       <div className={`flex items-center gap-[6px] ChartLabel ${className}`}>
         <span
-          className={`inline-flex w-3 h-3 bg-gradient-to-b ${thumbClass} ${
-            translucent
-              ? `opacity-30 from-${color}/50 to-${color}/30`
-              : `from-${color} to-${color}/10`
+          className={`inline-flex w-3 h-3 bg-gradient-to-b from- ${thumbClass} ${
+            translucent ? `opacity-30 from-primary/50 to-primary/30` : `from-primary to-primary/10`
           }`}
         />
         <p className="text-sm leading-none text-primary-light">{label}</p>
