df718d438f1a69af5928297895dbff41e0c1d270
dia.nn <dia.nn@quarkonix.com>
2023-11-21 11:10:43 +0900

    feat: update ChartLabel color

--- src/stories/atom/ChartLabel/index.tsx
+++ src/stories/atom/ChartLabel/index.tsx
@@ -18,8 +18,10 @@ export const ChartLabel = (props: ChartLabelProps) => {
     <SkeletonElement isLoading={isNotNil(isLoading) && isLoading} width={80}>
       <div className={`flex items-center gap-[6px] ChartLabel ${className}`}>
         <span
-          className={`inline-flex w-3 h-3 bg-gradient-to-b from-primary to-primary/10 !from-${color} !to-${color}/10 ${thumbClass} ${
-            translucent ? 'opacity-30' : ''
+          className={`inline-flex w-3 h-3 bg-gradient-to-b ${thumbClass} ${
+            translucent
+              ? `opacity-30 from-${color}/50 to-${color}/30`
+              : `from-${color} to-${color}/10`
           }`}
         />
         <p className="text-sm leading-none text-primary-light">{label}</p>
