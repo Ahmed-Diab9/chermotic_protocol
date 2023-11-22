cf75ee0e0321b7ac4ef21d730a84c15d0883a828
dia.nn <dia.nn@quarkonix.com>
2023-11-22 17:45:19 +0900

    feat: create CloasePositionModal ui

---
+++ src/stories/template/ClosePositionModal/__mocks__/hooks.tsx
@@ -0,0 +1,13 @@
+export function useClosePositonModal() {
+  return {
+    isOpen: true,
+    onClose: () => {},
+
+    tokenName: 'CHRM',
+
+    onClickAll: () => {},
+    onClickRemovable: () => {},
+
+    onClickSubmit: () => {},
+  };
+}
