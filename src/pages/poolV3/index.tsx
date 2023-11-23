b4a9d2784ecd16185676ed55e6272a00805b8c4b
austin-builds <austin.31@quarkonix.com>
2023-11-23 17:57:16 +0900

    refactor: essential components in each pages

--- src/pages/poolV3/index.tsx
+++ src/pages/poolV3/index.tsx
@@ -6,7 +6,6 @@ import { useCallback, useMemo } from 'react';
 import { toast } from 'react-toastify';
 import { PlusIcon } from '~/assets/icons/Icon';
 import useMarketLocal from '~/hooks/commons/useMarketLocal';
-import useBackgroundGradient from '~/hooks/useBackgroundGradient';
 import { useChromaticClient } from '~/hooks/useChromaticClient';
 import { useLpLocal } from '~/hooks/useLpLocal';
 import { useTokenLocal } from '~/hooks/useTokenLocal';
@@ -16,13 +15,7 @@ import { Avatar } from '~/stories/atom/Avatar';
 import { Button } from '~/stories/atom/Button';
 import { SkeletonElement } from '~/stories/atom/SkeletonElement';
 import { Tag } from '~/stories/atom/Tag';
-import { Toast } from '~/stories/atom/Toast';
-import { ChainModal } from '~/stories/container/ChainModal';
 import { MarketSelectV3 } from '~/stories/molecule/MarketSelectV3';
-import { BookmarkBoardV3 } from '~/stories/template/BookmarkBoardV3';
-import { Footer } from '~/stories/template/Footer';
-import { HeaderV3 } from '~/stories/template/HeaderV3';
-import { PoolAnalyticsV3 } from '~/stories/template/PoolAnalyticsV3';
 import { PoolDetail } from '~/stories/template/PoolDetail';
 import { PoolMenuV3 } from '~/stories/template/PoolMenuV3';
 import { PoolPanelV2 } from '~/stories/template/PoolPanelV2';
@@ -35,8 +28,6 @@ const PoolV3 = () => {
   useMarketLocal();
   useLpLocal();
   const { client } = useChromaticClient();
-  const { onLoadBackgroundRef } = useBackgroundGradient();
-
   const selectedLp = useAppSelector(selectedLpSelector);
   const lpTitle = isNotNil(selectedLp)
     ? `${selectedLp.settlementToken.name}-${selectedLp.market.description}`
@@ -95,107 +86,90 @@ const PoolV3 = () => {
 
   return (
     <>
-      <div className="page-container">
-        <div id="gradient" ref={(element) => onLoadBackgroundRef(element)}>
-          <div id="prev"></div>
-          <div id="current"></div>
+      <MarketSelectV3 />
+      <div className="flex items-stretch gap-5 mt-8">
+        <div className="flex-none w-[220px]">
+          <h4 className="mt-3 mb-2 text-left">Pools</h4>
+          <PoolMenuV3 />
         </div>
-        <BookmarkBoardV3 />
-        <HeaderV3 />
-        <main className="max-w-[1480px]">
-          <MarketSelectV3 />
-          <div className="flex items-stretch gap-5 mt-8">
-            <div className="flex-none w-[220px]">
-              <h4 className="mt-3 mb-2 text-left">Pools</h4>
-              <PoolMenuV3 />
-            </div>
+        <div className="flex-auto">
+          <div className="flex gap-3 mt-10 mb-5 text-left">
             <div className="flex-auto">
-              <div className="flex gap-3 mt-10 mb-5 text-left">
-                <div className="flex-auto">
-                  <div className="flex items-center mb-5">
-                    <SkeletonElement
-                      isLoading={isNil(lpTitle)}
-                      width={120}
-                      containerClassName="mr-3"
-                    >
-                      <h2 className="mr-3 text-4xl">
-                        {lpTitle} {selectedLp?.name}
-                      </h2>
-                    </SkeletonElement>
-                    <Tag label={selectedLp?.tag} className={tagClass} />
-                    <Button
-                      label="Metamask"
-                      iconLeft={<PlusIcon className="w-3 h-3" />}
-                      css="translucent"
-                      className="ml-4 !pl-2 !py-1"
-                      gap="1"
-                      size="sm"
-                      onClick={() => {
-                        onCLPRegister();
-                      }}
-                    />
-                  </div>
-                  <p className="mb-5 text-lg text-primary-light">{lpDescription}</p>
-                  {/* TODO: learn more button */}
-                </div>
-                <div className="flex-none w-[36%] min-w-[340px] max-w-[380px] flex items-end">
-                  <div className="flex items-center gap-2 ml-auto text-xl font-semibold">
-                    CLP Price
-                    <SkeletonElement isLoading={isNil(selectedLp)} containerClassName="w-[120px]">
-                      <Avatar
-                        label={`${price} ${selectedLp?.settlementToken.name}`}
-                        size="sm"
-                        gap="1"
-                        fontSize="2xl"
-                        src={selectedLp?.settlementToken.image}
-                      />
-                    </SkeletonElement>
-                  </div>
-                </div>
+              <div className="flex items-center mb-5">
+                <SkeletonElement isLoading={isNil(lpTitle)} width={120} containerClassName="mr-3">
+                  <h2 className="mr-3 text-4xl">
+                    {lpTitle} {selectedLp?.name}
+                  </h2>
+                </SkeletonElement>
+                <Tag label={selectedLp?.tag} className={tagClass} />
+                <Button
+                  label="Metamask"
+                  iconLeft={<PlusIcon className="w-3 h-3" />}
+                  css="translucent"
+                  className="ml-4 !pl-2 !py-1"
+                  gap="1"
+                  size="sm"
+                  onClick={() => {
+                    onCLPRegister();
+                  }}
+                />
               </div>
-
-              <div className="flex gap-3">
-                <div className="flex-auto overflow-hidden">
-                  <div className="panel panel-translucent">
-                    <PoolPanelV2 />
-                  </div>
-                  {/* TODO: Should be implemented later */}
-                  <div className="mt-10">
-                    <PoolAnalyticsV3 />
-                  </div>
-                </div>
-                <div className="flex-none w-[36%] min-w-[340px] max-w-[380px] flex flex-col gap-3">
-                  {/* To be added later */}
-                  {/* <PoolBalance /> */}
-                  <div className="panel panel-translucent">
-                    <PoolStat />
-                  </div>
-                  <div className="panel panel-translucent">
-                    <PoolPerformance />
-                  </div>
-                  <div className="panel panel-translucent">
-                    <PoolDetail />
-                  </div>
-                  <div className="mt-5">
-                    <Button
-                      to="/trade"
-                      css="translucent"
-                      label={
-                        isNotNil(marketDescription)
-                          ? `Trade on ${marketDescription} Pool`
-                          : 'Loading...'
-                      }
-                      iconRight={<ChevronRightIcon />}
-                    />
-                  </div>
-                </div>
+              <p className="mb-5 text-lg text-primary-light">{lpDescription}</p>
+              {/* TODO: learn more button */}
+            </div>
+            <div className="flex-none w-[36%] min-w-[340px] max-w-[380px] flex items-end">
+              <div className="flex items-center gap-2 ml-auto text-xl font-semibold">
+                CLP Price
+                <SkeletonElement isLoading={isNil(selectedLp)} containerClassName="w-[120px]">
+                  <Avatar
+                    label={`${price} ${selectedLp?.settlementToken.name}`}
+                    size="sm"
+                    gap="1"
+                    fontSize="2xl"
+                    src={selectedLp?.settlementToken.image}
+                  />
+                </SkeletonElement>
               </div>
             </div>
           </div>
-        </main>
-        <Footer />
-        <Toast />
-        <ChainModal />
+
+          <div className="flex gap-3">
+            <div className="flex-auto overflow-hidden">
+              <div className="panel panel-translucent">
+                <PoolPanelV2 />
+              </div>
+              {/* TODO: Should be implemented later */}
+              {/* <div className="mt-10">
+                    <PoolAnalyticsV3 />
+                  </div> */}
+            </div>
+            <div className="flex-none w-[36%] min-w-[340px] max-w-[380px] flex flex-col gap-3">
+              {/* To be added later */}
+              {/* <PoolBalance /> */}
+              <div className="panel panel-translucent">
+                <PoolStat />
+              </div>
+              <div className="panel panel-translucent">
+                <PoolPerformance />
+              </div>
+              <div className="panel panel-translucent">
+                <PoolDetail />
+              </div>
+              <div className="mt-5">
+                <Button
+                  to="/trade"
+                  css="translucent"
+                  label={
+                    isNotNil(marketDescription)
+                      ? `Trade on ${marketDescription} Pool`
+                      : 'Loading...'
+                  }
+                  iconRight={<ChevronRightIcon />}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/pages/tradeV3/index.tsx
+++ src/pages/tradeV3/index.tsx
@@ -1,13 +1,7 @@
 import useMarketLocal from '~/hooks/commons/useMarketLocal';
-import useBackgroundGradient from '~/hooks/useBackgroundGradient';
 import { usePositionFilterLocal } from '~/hooks/usePositionFilterLocal';
 import { useTokenLocal } from '~/hooks/useTokenLocal';
-import { Toast } from '~/stories/atom/Toast';
-import { ChainModal } from '~/stories/container/ChainModal';
 import { MarketSelectV3 } from '~/stories/molecule/MarketSelectV3';
-import { BookmarkBoardV3 } from '~/stories/template/BookmarkBoardV3';
-import { Footer } from '~/stories/template/Footer';
-import { HeaderV3 } from '~/stories/template/HeaderV3';
 import { TradeChartPanel } from '~/stories/template/TradeChartPanel';
 import { TradeManagementV3 } from '~/stories/template/TradeManagementV3';
 import { TradePanelV3 } from '~/stories/template/TradePanelV3';
@@ -18,33 +12,17 @@ function TradeV3() {
   useMarketLocal();
   usePositionFilterLocal();
 
-  const { beforeCondition, afterCondition, toggleConditions, onLoadBackgroundRef } =
-    useBackgroundGradient();
-
   return (
     <>
-      <div className="page-container !min-w-[1360px]">
-        <div id="gradient" ref={(element) => onLoadBackgroundRef(element)}>
-          <div id="prev"></div>
-          <div id="current"></div>
-        </div>
-        <BookmarkBoardV3 />
-        <HeaderV3 />
-        <main>
-          <div className="flex w-full gap-3 overflow-hidden">
-            <article className="flex flex-col flex-auto w-full gap-5">
-              <div className="pb-2">
-                <MarketSelectV3 />
-              </div>
-              <TradeChartPanel />
-              <TradeManagementV3 />
-            </article>
-            <TradePanelV3 />
+      <div className="flex w-full gap-3 overflow-hidden">
+        <article className="flex flex-col flex-auto w-full gap-5">
+          <div className="pb-2">
+            <MarketSelectV3 />
           </div>
-        </main>
-        <Footer />
-        <Toast />
-        <ChainModal />
+          <TradeChartPanel />
+          <TradeManagementV3 />
+        </article>
+        <TradePanelV3 />
       </div>
     </>
   );
