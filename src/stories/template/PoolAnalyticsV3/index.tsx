96b67d624fd6b2c3a865810200be3bd22f1efe64
dia.nn <dia.nn@quarkonix.com>
2023-11-23 19:32:23 +0900

    design: ui detail margin

--- src/stories/template/PoolAnalyticsV3/index.tsx
+++ src/stories/template/PoolAnalyticsV3/index.tsx
@@ -27,13 +27,13 @@ export const PoolAnalyticsV3 = (props: PoolAnalyticsV3Props) => {
       </div>
       <div className="panel panel-translucent">
         <div className="p-5 text-left">
-          <h3>CLP Price</h3>
+          <h3 className="mb-5">CLP Price</h3>
           {isLoading ? <TemporalLoadingComponent /> : <AnalyticsChart {...priceChartProps} />}
         </div>
       </div>
       <div className="panel panel-translucent">
         <div className="p-5 text-left">
-          <h3>AUM (Assets under Management) & CLP Supply</h3>
+          <h3 className="mb-5">AUM (Assets under Management) & CLP Supply</h3>
           {isLoading ? <TemporalLoadingComponent /> : <AnalyticsChart {...supplyChartProps} />}
         </div>
       </div>
