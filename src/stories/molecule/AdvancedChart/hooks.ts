2b64363b7c3aeb4e56bfb0af6580f6972d97dc0d
jaycho-46 <jay.46@quarkonix.com>
2023-11-29 17:41:14 +0900

    refactor: remove unused lines

--- src/stories/molecule/AdvancedChart/hooks.ts
+++ src/stories/molecule/AdvancedChart/hooks.ts
@@ -10,7 +10,6 @@ import {
   widget,
 } from '~/lib/charting_library';
 import datafeed from '~/lib/pyth/datafeed';
-import { useAppDispatch } from '~/store';
 
 import { numberFormat } from '~/utils/number';
 import { changeTheme } from './utils';
@@ -21,7 +20,6 @@ export const useAdvancedChart = (props: AdvancedChartProps) => {
   const { symbol, darkMode } = props;
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [isChartReady, setIsChartReady] = useState<boolean>(false);
-  const dispatch = useAppDispatch();
 
   const chartContainerRef = useRef<HTMLDivElement>() as React.MutableRefObject<HTMLInputElement>;
 
