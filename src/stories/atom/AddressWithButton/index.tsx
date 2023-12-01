10d0dd3782733c161e3706b6aab2fb7e524202df
dia.nn <dia.nn@quarkonix.com>
2023-12-01 13:51:14 +0900

    feat: add AddressWithButton size prop

--- src/stories/atom/AddressWithButton/index.tsx
+++ src/stories/atom/AddressWithButton/index.tsx
@@ -4,18 +4,20 @@ import Skeleton from 'react-loading-skeleton';
 import { OutlinkIcon } from '~/assets/icons/Icon';
 import { Button } from '../Button';
 import { TooltipGuide } from '../TooltipGuide';
+import './style.css';
 
 interface AddressWithButtonProps {
   address?: string;
   disabled?: boolean;
   icon?: 'copy' | 'outlink';
+  size?: 'base' | 'lg' | 'xl';
   className?: string;
   onClick: MouseEventHandler<HTMLButtonElement>;
   href?: string;
 }
 
 export const AddressWithButton = (props: AddressWithButtonProps) => {
-  const { address, icon = 'copy', onClick, className, href } = props;
+  const { address, icon = 'copy', size = 'lg', onClick, className, href } = props;
   const hrefProps = useMemo(() => {
     switch (icon) {
       case 'copy': {
@@ -31,22 +33,13 @@ export const AddressWithButton = (props: AddressWithButtonProps) => {
 
   return (
     <>
-      <div
-        className={`flex items-center justify-between flex-auto border dark:border-transparent border-collapse rounded-full w-fit bg-paper dark:bg-paper-light text-primary ${className}`}
-      >
-        <p className="w-[calc(100%-40px)] px-4 overflow-hidden min-w-[100px] text-left">
-          {address ? <>{address}</> : <Skeleton width="100%" />}
-        </p>
+      <div className={`AddressWithButton w-fit ${className}`}>
+        <p className="wrapper-address">{address ? <>{address}</> : <Skeleton width="100%" />}</p>
         {icon === 'copy' && <TooltipGuide tipOnly tip="Copy" label="copy-address" />}
-        {/* <TooltipGuide
-          tipOnly
-          tip={button === 'copy' ? 'Copy' : button === 'outlink' ? 'Open in a new tab' : ''}
-          label="copy-address"
-        /> */}
         <Button
           label="Copy Address"
           css="light"
-          size="lg"
+          size={size}
           className="m-[-1px] tooltip-copy-address"
           iconOnly={
             icon === 'copy' ? <Square2StackIcon /> : icon === 'outlink' ? <OutlinkIcon /> : null
